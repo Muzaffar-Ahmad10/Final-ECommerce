76a5eb73e85a9545386a7709ba724af8bf835968
Muzaffar Tahir <muzaffarahmad709@gmail.com>
2024-07-29 08:07:22 +0000

    product model updated for minor changes

--- products/models.py
+++ products/models.py
@@ -27,6 +27,6 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product_images/')
     alt_text = models.CharField(max_length=255, blank=True, null=True)
-    
+
     def __str__(self):
-        return f"Image for {self.product.name}"
+        return self.alt_text or f"Image for {self.product.name}"
